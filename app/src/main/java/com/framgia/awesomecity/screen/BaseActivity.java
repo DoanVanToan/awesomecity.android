e65d1883cc692fae6684e7d76e952936e40c563f
ducbet <ducbet1996@gmail.com>
2017-07-19 10:25:15 +0700

    init_project

---
+++ app/src/main/java/com/framgia/awesomecity/screen/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.framgia.awesomecity.screen;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tmd on 19/07/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
